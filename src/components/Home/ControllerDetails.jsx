5d88811a17911858812a25ff61d62c3576926aac
bharath-designer <bharathv.developer@gmail.com>
2025-02-21 17:12:17 +0530

    responsive added

--- src/components/Home/ControllerDetails.jsx
+++ src/components/Home/ControllerDetails.jsx
@@ -8,30 +8,30 @@ import controller3 from "../../assets/images/controller-last.jpg";
 const ControllerDetails = () => {
   return (
     <div
-    id="accessories"
+      id="accessories"
       className={`${styles["container"]} ${layoutStyles["container-full-screen"]}`}
     >
-      <div className={styles["headings"]}>
-        <div className={styles["heading"]}>DualSense 2 Controller</div>
-        <div className={styles["heading"]}>The Ultimate Gaming Companion</div>
-      </div>
-
-      <div className={styles["desc"]}>
-        Feel Every Moment, Control Every Move
-      </div>
-
-      <div className={styles["images-container"]}>
-        <div className={styles["img-content-container"]}>
-          <div className={styles["img-content"]}>
-            <img src={controller1} alt="" />
-          </div>
-          <div className={styles["img-content"]}>
-            <img src={controller2} alt="" />
-          </div>
-          <div className={styles["img-content"]}>
-            <img src={controller3} alt="" />
-          </div>
+        <div className={styles["headings"]}>
+          <div className={styles["heading"]}>DualSense 2 Controller</div>
+          <div className={styles["heading"]}>The Ultimate Gaming Companion</div>
         </div>
+
+        <div className={styles["desc"]}>
+          Feel Every Moment, Control Every Move
+        </div>
+
+        <div className={styles["images-container"]}>
+          <div className={styles["img-content-container"]}>
+            <div className={styles["img-content"]}>
+              <img src={controller1} alt="" />
+            </div>
+            <div className={styles["img-content"]}>
+              <img src={controller2} alt="" />
+            </div>
+            <div className={styles["img-content"]}>
+              <img src={controller3} alt="" />
+            </div>
+          </div>
       </div>
     </div>
   );
